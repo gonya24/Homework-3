cca50051d8ab5f8b03d872c2e46d8eacecab41ad
Illia <illia.gonya@icloud.com>
2023-04-02 19:55:07 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,3 @@
+var x1 = prompt ('enter first number'); // Введение первого числа
+var x2 = prompt ('enter second number'); // Введение второго числа
+alert (x1 + '+' + x2 + '=' + (Number(x1)+Number(x2)) + '\n' + x1 + '-' + x2 + '=' + (x1-x2)+ '\n' + x1 + '*' + x2 + '=' + (x1*x2)+ '\n' + x1 + '/' + x2 + '=' + (x1/x2)); // Результат 
